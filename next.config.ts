f43d5ffe34f8ec16a8122c215c12fce3f1e20177
Saar-engflow <robinsdev2@gmail.com>
2026-06-23 10:35:05 +0200

    fix/use-turbopack-for-next16

--- next.config.ts
+++ next.config.ts
@@ -1,57 +1,8 @@
 import type { NextConfig } from "next";
-import withPWA from "@ducanh2912/next-pwa";
 
-const pwaConfig = withPWA({
-  dest: "public",
-  cacheOnFrontendNav: true,
-  aggressiveFrontEndNavCaching: true,
-  reloadOnOnline: true,
-  swMinify: true,
-  disable: process.env.NODE_ENV === "development",
-  workboxOptions: {
-    disableDevLogs: true,
-    runtimeCaching: [
-      {
-        urlPattern: /^https:\/\/fonts\.(?:googleapis|gstatic)\.com\/.*/i,
-        handler: "CacheFirst",
-        options: {
-          cacheName: "google-fonts",
-          cacheableResponse: {
-            statuses: [0, 200],
-          },
-        },
-      },
-      {
-        urlPattern: /^https:\/\/.*\.(?:png|jpg|jpeg|svg|ico|webp)$/i,
-        handler: "CacheFirst",
-        options: {
-          cacheName: "static-assets",
-          cacheableResponse: {
-            statuses: [0, 200],
-          },
-        },
-      },
-      {
-        urlPattern: /\/api\/.*/i,
-        handler: "NetworkOnly",
-      },
-      {
-        urlPattern: /\/.*/i,
-        handler: "NetworkFirst",
-        options: {
-          cacheName: "app-shell",
-          cacheableResponse: {
-            statuses: [0, 200],
-          },
-        },
-      },
-    ],
-  },
-});
-
-const nextConfig: NextConfig = pwaConfig({
-  webpack: {},
+const nextConfig: NextConfig = {
+  turbopack: {},
   /* config options here */
-});
+};
 
 export default nextConfig;
